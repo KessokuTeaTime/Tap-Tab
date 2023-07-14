a8a57a2742dd476c400513ad6a697da1d6a662f0
KrLite <68179735+KrLite@users.noreply.github.com>
2023-07-14 10:54:00 +0800

    2.2.0

--- src/main/java/net/krlite/taptab/mixin/InGameHudAnimator.java
+++ src/main/java/net/krlite/taptab/mixin/InGameHudAnimator.java
@@ -1,7 +1,7 @@
 package net.krlite.taptab.mixin;
 
 import net.krlite.taptab.InventorySwapper;
-import net.krlite.taptab.TapTabClient;
+import net.krlite.taptab.TapTab;
 import net.minecraft.client.gui.hud.InGameHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,13 +22,13 @@ public abstract class InGameHudAnimator {
 			)
 	)
 	private void renderHotbarItem(Args args /* MatrixStack *, int x, int y, float tickDelta, PlayerEntity *, ItemStack *, int seed */ ) {
-		int x = args.get(2), y = args.get(3), slot = (x - 2 + 90 - scaledWidth / 2) / 20;
+		int x = args.get(1), y = args.get(2), slot = (x - 2 + 90 - scaledWidth / 2) / 20;
 		long start = InventorySwapper.HOTBAR_SLOTS_ANIMATION_START[slot];
-		double progress = (double) Math.max((System.currentTimeMillis() - start), 0) / TapTabClient.ANIMATION_DURATION;
+		double progress = (double) Math.max((System.currentTimeMillis() - start), 0) / TapTab.ANIMATION_DURATION;
 
 		if (start == -1 || progress > 1) return;
 
 		boolean reversed = InventorySwapper.HOTBAR_SLOTS_ANIMATION_REVERSED[slot];
-		args.set(3, y + (int) TapTabClient.easeOutBounce(progress, reversed));
+		args.set(2, y + (int) TapTab.easeOutBounce(progress, reversed));
 	}
 }
