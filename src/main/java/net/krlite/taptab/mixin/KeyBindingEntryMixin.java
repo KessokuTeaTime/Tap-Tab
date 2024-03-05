8a0df84fe8212aae53f47c824d551fc71a28d109
KrLite <68179735+KrLite@users.noreply.github.com>
2024-03-05 18:16:19 +0800

    use injects

--- src/main/java/net/krlite/taptab/mixin/KeyBindingEntryMixin.java
+++ src/main/java/net/krlite/taptab/mixin/KeyBindingEntryMixin.java
@@ -2,30 +2,33 @@ package net.krlite.taptab.mixin;
 
 import net.krlite.taptab.TooltipKeyBinding;
 import net.minecraft.client.gui.screen.option.ControlsListWidget;
-import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.option.KeyBinding;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ControlsListWidget.KeyBindingEntry.class)
 public class KeyBindingEntryMixin {
     @Shadow @Final private KeyBinding binding;
 
-    @Redirect(
+    @Shadow @Final private ButtonWidget editButton;
+
+    @Inject(
             method = "update",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/client/gui/widget/ButtonWidget;setTooltip(Lnet/minecraft/client/gui/tooltip/Tooltip;)V",
-                    ordinal = 1
+                    ordinal = 1,
+                    shift = At.Shift.AFTER
             )
     )
-    private void appendTooltip(ButtonWidget buttonWidget, Tooltip tooltip) {
+    private void appendTooltip(CallbackInfo ci) {
         if (binding instanceof TooltipKeyBinding tooltipKeyBinding) {
-            buttonWidget.setTooltip(tooltipKeyBinding.tooltipSupplier().get());
+            editButton.setTooltip(tooltipKeyBinding.tooltipSupplier().get());
         }
     }
 }
